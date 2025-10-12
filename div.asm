0c3f292dcf4f77a21af90dcdeebd48fd6b5ae205
Minhajul Islam <mmminhaj221@gmail.com>
2025-10-13 00:01:44 +0600

    Create div.asm

---
+++ div.asm
@@ -0,0 +1,30 @@
+.model small
+.stack 100h
+.data
+.code
+main proc
+mov ah , 1
+int 21h
+
+sub al , 48
+mov bl , al 
+
+mov ah , 1
+int 21h
+
+sub al , 48
+mov bh , al 
+
+mov al , bl
+mov ah  ,0
+
+div bh
+
+mov dl , al
+add dl , 48
+mov ah , 2
+int 21h
+
+main endp
+end main
+
